2ecec0f66997d8a8ca43efb60902c3872d335344
SCOPE <olajuwonrichard22@gmail.com>
2025-04-13 18:09:58 +0100

    updated readme.md and server url

--- client/src/constant.js
+++ client/src/constant.js
@@ -1,3 +1,3 @@
 //export const serverUrl = "http://localhost:3001";
 
-export const serverUrl = "https://socialize-cshw.onrender.com";
+export const serverUrl = "https://socialize-0hs7.onrender.com";
